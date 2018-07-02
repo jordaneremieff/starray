c39a87da62d4a10d14f439d41006089036919cdd
erm <jermff@gmail.com>
2018-07-03 01:56:05 +1000

    Streaming without websockets

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from afiqah import Afiqah
 from starlette import HTMLResponse
 from jinja2 import Environment, FileSystemLoader
-from starray import WebSocketApp
+from starray import WebSocketApp, StreamApp
 
 env = Environment(loader=FileSystemLoader("templates"))
-template = env.get_template("index.html")
 
 
 app = Afiqah()
@@ -12,9 +11,21 @@ app = Afiqah()
 
 @app.route("/")
 def homepage(scope):
+    template = env.get_template("index.html")
     return HTMLResponse(template.render())
 
 
 @app.route("/ws")
 def pyray_ws(scope):
     return WebSocketApp(scope)
+
+
+@app.route("/streampage/")
+def streampage(scope):
+    template = env.get_template("stream.html")
+    return HTMLResponse(template.render())
+
+
+@app.route("/stream/")
+def pyray_stream(scope):
+    return StreamApp(scope)
--- starray.py
+++ starray.py
@@ -39,3 +39,57 @@ class WebSocketApp(WebSocketConsumer):
             await self.send({"type": "websocket.send", "bytes": img_bytes.getvalue()})
 
         await self.send({"type": "websocket.close", "code": 1000})
+
+
+class StreamApp:
+
+    def __init__(self, scope):
+        self.scope = scope
+
+    async def __call__(self, receive, send):
+        message = await receive()
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    [b"Content-Type", b"multipart/x-mixed-replace; boundary=frame"]
+                ],
+            }
+        )
+        async for data in stream():
+            await send({"type": "http.response.body", "body": data, "more_body": True})
+            await asyncio.sleep(0.1)
+
+
+async def stream():
+    number_of_circles = 20
+    line_thickness = 2
+
+    for i in np.arange(60):
+        r = rotation(
+            3, 2.5 + np.pi * np.sin(i / 10.0) * np.random.uniform(0.75, 1) / 20.0
+        )
+        im = Image.new("RGB", (500, 500), (1, 1, 1))
+        draw = ImageDraw.Draw(im, "RGBA")
+        draw_sphere(
+            draw,
+            np.array([0, 0, 0]),
+            np.array([0, 0, 1]),
+            1 * np.random.uniform(0.75, 1) + 0.4 * np.sin(np.pi / 10.0 * i),
+            r,
+            num_circle=number_of_circles,
+            rgba=(182, 183, 186, 255),
+            width=line_thickness,
+        )
+        img_bytes = io.BytesIO()
+        im.save(img_bytes, format="PNG")
+
+        yield b"".join(
+            [
+                b"--frame\r\n",
+                b"Content-Type: image/jpeg\r\n\r\n",
+                img_bytes.getvalue(),
+                b"\r\n",
+            ]
+        )
